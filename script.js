7d6c21e4c6c8cf789e6a454a09a0379064c7a55e
Ile16 <ile.coc95@outlook.com>
2024-05-05 10:05:48 +0200

    add

--- script.js
+++ script.js
@@ -5,16 +5,40 @@ const urlUsers = "https://jsonplaceholder.typicode.com/users"
 const selected = document.getElementById("inputSelezionato");
 //puntatore dell'input inserito 
 const inputText = document.getElementById("inputInserito");
-//puntatore della tabella che verrà popolata di tutti i dati
-const tableData = document.getElementById("users-data");
+//puntatore della tabella che verrà popolata da tutti i dati
+const tableData = document.getElementById("usersData");
 
 
-//funzione asincrona
+//funzione asincrona - richiesta dati 
 async function fetchData() {
     const response = await fetch(urlUsers); //richiedo i dati
     const users = await response.json(); //dati in formato json 
-    console.log(users);
+    //console.log(users);
     return users;
 }
 
-fetchData() //richiamare la funzione per vedere in console l'array
+//fetchData() //richiamare la funzione per vedere in console l'array
+
+//funzione che stampa i dati 
+function datiUtenti(users) {
+    //pulisco prima la pagina
+    tableData.innerHTML = " ";
+    //ciclo della risposta fetch per creare un elemento tr ad ogni dato
+    users.forEach(user => {
+        //variabile creazione elemento per riga
+        const tableRow = document.createElement("tr");
+        //riempio hatml con constante tableRow
+        tableRow.innerHTML = `
+        <td>${user.id} </td>
+        <td>${user.name} </td>
+        <td>${user.username} </td>
+        <td>${user.email.toLowerCase()} </td>
+        `;
+        //appenChild per appendere il nuovo elemento creato alla costante con classe tbody
+        tableData.appendChild(tableRow);
+    });
+}
+
+fetchData(datiUtenti(users));
+
+
